a51f1068271b620bca46fef26ec0e27390fd39dc
jthomaz <jthomaz@brasilcap.com.br>
2018-04-12 19:04:16 -0300

    Separando a chamada das telas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  exibeCadastro : boolean = false;
+
+  ngOnInit() {
+    this.exibeCadastro = false;
+
+  }
+
+  troca(){
+    this.exibeCadastro = !this.exibeCadastro;
+  }
+
 }
